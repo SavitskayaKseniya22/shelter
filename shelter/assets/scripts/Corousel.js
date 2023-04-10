62e385a66c1cdae6d3c9d6db93d8ae29022a312e
Kseniia Savitskaia <savitskaya.kseniya.22@yandex.ru>
2023-04-10 16:22:01 +0300

    feat: make buffer for carousel

--- shelter/assets/scripts/Corousel.js
+++ shelter/assets/scripts/Corousel.js
@@ -1,22 +1,26 @@
-import { shuffle, makeAnimalCardsList, render, checkOffset } from "./utilities.js";
+import {
+    shuffle,
+    makeAnimalCardsList,
+    render,
+    checkOffset,
+} from "./utilities.js";
 
 export class Corousel {
     constructor() {
         this.cardsFromData = makeAnimalCardsList(1);
-        shuffle(this.cardsFromData)
+        shuffle(this.cardsFromData);
         this.activeCards = this.cardsFromData.slice(0, 3);
-        this.activeContent = this.makeContentFromCard(this.activeCards)
+        this.activeContent = this.makeContentFromCard(this.activeCards);
         this.nextCards = this.cardsFromData.slice(3, 6);
-        this.nextContent = this.makeContentFromCard(this.activeCards)
+        this.nextContent = this.makeContentFromCard(this.activeCards);
         this.previousCards = this.cardsFromData.slice(5, 8);
-        this.previousContent = this.makeContentFromCard(this.activeCards)
-        render(".friends-list__active", this.activeContent, "replacement")
-        render(".friends-list__next", this.nextContent, "replacement")
-        render(".friends-list__previous", this.previousContent, "replacement")
-        this.addListener()
-
-
-
+        this.previousContent = this.makeContentFromCard(this.activeCards);
+        this.buffer = [this.activeCards];
+        render(".friends-list__active", this.activeContent, "replacement");
+        render(".friends-list__next", this.nextContent, "replacement");
+        render(".friends-list__previous", this.previousContent, "replacement");
+        this.addListener();
+        this.sequence = "";
     }
 
     makeContentFromCard(cards) {
@@ -31,70 +35,75 @@ export class Corousel {
                     j++;
                 }
             }
-            return j == 3 ? true : false
-
-        })
+            return j == 3 ? true : false;
+        });
         shuffle(filteredCards);
-        return filteredCards.slice(0, 3)
+        return filteredCards.slice(0, 3);
     }
 
     addListener() {
-        document.addEventListener('click', (event) => {
-
+        document.addEventListener("click", (event) => {
             if (event.target.closest(".friends-corousel-button__right")) {
-                this.offset = checkOffset()
-                this.nextCards = this.makeAnotherPart();
+                this.offset = checkOffset();
+
+                this.sequence += ">";
+
+                if (this.sequence.slice(-2) == "<>") {
+                    this.nextCards = this.buffer[this.buffer.length - 1];
+                } else {
+                    this.nextCards = this.makeAnotherPart();
+                }
+
                 this.nextContent = this.makeContentFromCard(this.nextCards);
-                render(".friends-list__next", this.nextContent, "replacement")
+                render(".friends-list__next", this.nextContent, "replacement");
 
-                this.buffer = this.activeContent;
+                this.buffer.push(this.activeCards);
 
-
-                document.querySelector(".friends-list__active").style = `transform: translateX(-${this.offset}); transition: transform 1s`;
-                document.querySelector(".friends-list__next").style = `transform: translateX(-${this.offset}); transition: transform 1s`;
+                document.querySelector(
+                    ".friends-list__active"
+                ).style = `transform: translateX(-${this.offset}); transition: transform 1s`;
+                document.querySelector(
+                    ".friends-list__next"
+                ).style = `transform: translateX(-${this.offset}); transition: transform 1s`;
 
                 setTimeout(() => {
-                    render(".friends-list__active", this.nextContent, "replacement")
+                    render(".friends-list__active", this.nextContent, "replacement");
                     document.querySelector(".friends-list__active").style = "";
                     document.querySelector(".friends-list__next").style = "";
                 }, "1000");
 
                 this.activeCards = this.nextCards;
-
-
-
-
             } else if (event.target.closest(".friends-corousel-button__left")) {
-                this.offset = checkOffset()
-                this.previousCards = this.makeAnotherPart();
+                this.offset = checkOffset();
+
+                this.sequence += "<";
+
+                if (this.sequence.slice(-2) == "><") {
+                    this.previousCards = this.buffer[this.buffer.length - 1];
+                } else {
+                    this.previousCards = this.makeAnotherPart();
+                }
 
                 this.previousContent = this.makeContentFromCard(this.previousCards);
-                render(".friends-list__previous", this.previousContent, "replacement")
+                render(".friends-list__previous", this.previousContent, "replacement");
 
+                this.buffer.push(this.activeCards);
 
-                this.buffer = this.activeContent;
-
-
-                document.querySelector(".friends-list__active").style = `transform: translateX(${this.offset}); transition: transform 1s`;
-                document.querySelector(".friends-list__previous").style = `transform: translateX(${this.offset}); transition: transform 1s`;
+                document.querySelector(
+                    ".friends-list__active"
+                ).style = `transform: translateX(${this.offset}); transition: transform 1s`;
+                document.querySelector(
+                    ".friends-list__previous"
+                ).style = `transform: translateX(${this.offset}); transition: transform 1s`;
 
                 setTimeout(() => {
-                    render(".friends-list__active", this.previousContent, "replacement")
+                    render(".friends-list__active", this.previousContent, "replacement");
                     document.querySelector(".friends-list__active").style = "";
                     document.querySelector(".friends-list__previous").style = "";
                 }, "1000");
 
                 this.activeCards = this.previousCards;
             }
-
-
         });
-
-
-
-
-
-
     }
-
 }
