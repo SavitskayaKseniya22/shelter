7de13367d77855b09f392c7e5f98cb653fccbc2b
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2023-12-06 16:43:55 +0200

    feat: remove old files and transfer assets into public folder

--- shelter/pages/main/script.js
+++
@@ -1,5 +0,0 @@
-import { Corousel } from "../../assets/scripts/Corousel.js";
-import { Header } from "../../assets/scripts/Header.js";
-
-new Header();
-new Corousel();
--- shelter/pages/pets/script.js
+++
@@ -1,5 +0,0 @@
-import { Pagination } from "../../assets/scripts/Pagination.js";
-import { Header } from "../../assets/scripts/Header.js";
-
-new Header()
-new Pagination()
