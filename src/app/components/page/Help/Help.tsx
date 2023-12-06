069a6a7c60cd8b6a5752aad67ddcb5193d27be10
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2023-12-07 00:21:49 +0200

    refactor: Help

--- src/app/components/page/Help/Help.tsx
+++ src/app/components/page/Help/Help.tsx
@@ -2,100 +2,39 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './help.module.scss';
 
+const data = [
+  { title: 'Pet food', src: '/icons/icon-pet-food.svg' },
+  { title: 'Transportation', src: '/icons/icon-transportation.svg' },
+  { title: 'Toys', src: '/icons/icon-toys.svg' },
+  { title: 'Bowls and cups', src: '/icons/icon-bowls-and-cups.svg' },
+  { title: 'Shampoos', src: '/icons/icon-shampoos.svg' },
+  { title: 'Vitamins', src: '/icons/icon-vitamins.svg' },
+  { title: 'Medicines', src: '/icons/icon-medicines.svg' },
+  { title: 'Collars / leashes', src: '/icons/icon-collars-leashes.svg' },
+  { title: 'Sleeping areas', src: '/icons/icon-sleeping-area.svg' },
+];
+
+function HelpItem({ title, src }: { title: string; src: string }) {
+  return (
+    <li className={styles['help-item']}>
+      <Image width={60} height={60} src={src} alt={title} />
+      <h3>{title}</h3>
+    </li>
+  );
+}
+
 function Help() {
   return (
-    <section id="help" className={styles.help}>
+    <section id="help">
       <div className={`${styles.container} container`}>
         <h2>
           How you can help <br />
           our shelter
         </h2>
         <ul className={styles['help-list']}>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-pet-food.svg"
-              alt="pet food"
-            />
-            <h3>Pet food</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-transportation.svg"
-              alt="transportation"
-            />
-            <h3>Transportation</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-toys.svg"
-              alt="toys"
-            />
-            <h3>Toys</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-bowls-and-cups.svg"
-              alt="bowls and cups"
-            />
-
-            <h3>Bowls and cups</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-shampoos.svg"
-              alt="shampoos"
-            />
-
-            <h3>Shampoos</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-vitamins.svg"
-              alt="vitamins"
-            />
-            <h3>Vitamins</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-medicines.svg"
-              alt="medicines"
-            />
-
-            <h3>Medicines</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-collars-leashes.svg"
-              alt="collars and leashes"
-            />
-            <h3>Collars / leashes</h3>
-          </li>
-          <li>
-            <Image
-              width={60}
-              height={60}
-              src="/icons/icon-sleeping-area.svg"
-              alt="sleeping area"
-            />
-
-            <h3>Sleeping areas</h3>
-          </li>
+          {data.map((item) => (
+            <HelpItem title={item.title} src={item.src} key={item.title} />
+          ))}
         </ul>
       </div>
     </section>
