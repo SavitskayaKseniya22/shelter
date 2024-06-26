d2075096118e78f9bdf52b819614bddbda838082
SavitskayaKseniya22 <savitskaya.kseniya.22@yandex.ru>
2024-06-26 13:11:04 +0300

    refactor: make some components server

--- loading.tsx
+++ loading.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import Spinner from '@/app/components/shared/Spinner/Spinner';
 
--- src/app/pets/page.tsx
+++ src/app/pets/page.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import styles from './page.module.scss';
 import Pagination from './pagination/Pagination';
